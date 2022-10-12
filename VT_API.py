d81478903dab88a7b04d667f386b925ea282ba0a
joshua <tanjunqing.joshua@gmail.com>
2022-10-12 16:22:50 +0800

    Removed interactive mode, condensed VT lookups

--- VT_API.py
+++ VT_API.py
@@ -3,49 +3,68 @@ import config
 import requests
 import json
 
-def api_call_virustotal(ip_address):
+def api_call_virustotal(query):
     
-    ip_address = ip_address
     api_key = config.virustotal_api_key
-    api_url = "https://www.virustotal.com/api/v3/search?query=" + ip_address
+    api_url = "https://www.virustotal.com/api/v3/search?query=" + query
     headers = {
         "Accept": "application/json",
         "x-apikey": config.virustotal_api_key
     }
     response = requests.get(api_url, headers=headers)
+    
     return response
 
-def read_response(response):
+
+def read_response(response, query):
     '''
     returns a dictionary with flagged ratio, engines flagged and quantity for each reponse
     '''
+    #pull information
     j_data = json.loads(response.text)
     info = j_data['data']
-    stats = info[0]['attributes']['last_analysis_stats']
+    
+    #stats infomation from info
+    try:
+        stats = info[0]['attributes']['last_analysis_stats']
+        
+    except IndexError:
+        #exit when 
+        print(f"No valid results for {query}")
+        return {'query_input': query
+               }
 
     harmless = stats.get('harmless', 0)
     malicious = stats.get('malicious', 0)
     suspicious = stats.get('suspicious', 0)
     undetected = stats.get('undetected', 0)
-    timeout = stats.get("timeout", 0)
-
+    timeout = stats.get('timeout', 0)
     stats_output = (malicious + suspicious) / (malicious + suspicious + harmless + undetected + timeout)
-
-    info = j_data['data']
+    
+    
+    #getting engine names from info
     data = info[0]['attributes']['last_analysis_results']
 
-    new_list = []
+    mal_list = []
     for i in data:
         if data[i]['category'] == 'malicious':
-            new_list.append(data[i]['engine_name'])
+            mal_list.append(data[i]['engine_name'])
     
-    out = {'stats_output': stats_output,
-           'engines_flagged': new_list,
+    out = {'query_input': query,
+           'stats_output': stats_output,
+           'engines_flagged': mal_list,
            'harmless': harmless,
            'malicious': malicious,
            'suspicious': suspicious,
            'undetected': undetected,
            'timeout':timeout
           }
+    
+    return out
+
+
+def virustotal_request(query):
+    response = api_call_virustotal(query)
+    out = read_response(response, query)
     
     return out
